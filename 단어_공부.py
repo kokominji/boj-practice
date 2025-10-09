78dbabfda703886da4772c366cd3d165328117aa
kokominji <kmj6386@gmail.com>
2025-10-10 04:00:04 +0900

    10/10

--- 단어_공부.py
+++ 단어_공부.py
@@ -1,2 +1,10 @@
+# 하나씩 순회하면서 같은 문자열을 카운트
+# 거기서 max값인 문자열을 upper로 반환
 world = str(input())
+count = 0
 
+for i in world:
+    if (i == world):
+        count += 1
+    print(max(count))
+        
---
+++ 상수.py
@@ -0,0 +1,6 @@
+a, b = input().split()
+
+A = int(a[::-1])
+B = int(b[::-1])
+
+print(max(A, B))
